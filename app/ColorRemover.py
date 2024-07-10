42dae79933d81f9b8163a0d7f3dab392ac13b5d5
semnisem <doldori102493@gmail.com>
2024-07-10 15:51:32 +0900

    S3 연동

--- app/ColorRemover.py
+++ app/ColorRemover.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
+import io
+
 
 def rgb_to_hsv(r, g, b):
     color = np.uint8([[[r, g, b]]])
     hsv_color = cv2.cvtColor(color, cv2.COLOR_RGB2HSV)
     return hsv_color[0][0]
 
+
 def to_hsv(image_rgb):
     image_hsv = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2HSV)
     return image_hsv
+
 
 class ColorRemover:
     def __init__(self, target_rgb, tolerance=40):
@@ -17,17 +21,6 @@ class ColorRemover:
         self.target_hsv = rgb_to_hsv(*target_rgb)
         self.alpha_channel = None
         self.masks = None
-        print(cv2.version)
-        print(np.version)
-
-    def remove_alpha(self, image):
-        if image.shape[2] == 4:  # RGBA
-            self.alpha_channel = image[:, :, 3]
-            image_rgb = image[:, :, :3]
-        else:
-            image_rgb = image
-
-        return image_rgb
 
     def combine_alpha(self, image_rgb):
         if self.alpha_channel is not None:  # RGBA
@@ -36,30 +29,14 @@ class ColorRemover:
         else:
             return image_rgb
 
-    def open(self, image_path):
-        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-        image_rgb = self.remove_alpha(image)
-
-        return image_rgb
-
-    def mask(self, image_rgb, mask_path):
+    def mask(self, image_rgb, mask_path=None):
         image_hsv = to_hsv(image_rgb)
         lower_bound = np.array([self.target_hsv[0] - self.tolerance, 50, 50])
         upper_bound = np.array([self.target_hsv[0] + self.tolerance, 255, 255])
 
         self.masks = cv2.inRange(image_hsv, lower_bound, upper_bound)
-        cv2.imwrite(mask_path, self.masks)
 
-    def fill(self, image_rgb):
         image_rgb[self.masks != 0] = [255, 255, 255]
-
-        return image_rgb
-
-    def blur(self, image_rgb, kernel_size=(601, 601)):
-        mask_3d = self.masks[:, :, None] == 0
-        blurred_image = cv2.GaussianBlur(image_rgb, kernel_size, 0)
-        image_rgb = np.where(mask_3d, image_rgb, blurred_image)
-
         return image_rgb
 
     def inpaint(self, image_rgb):
@@ -69,43 +46,34 @@ class ColorRemover:
         else:
             raise ValueError("Mask is not properly defined or is not a numpy array.")
 
-    def save(self, image_rgb, output_path):
-        image = self.combine_alpha(image_rgb)
-        cv2.imwrite(output_path, image)
+    def remove_alpha(self, image):
+        if image.shape[2] == 4:  # RGBA or RGB
+            self.alpha_channel = image[:, :, 3]
+            image_rgb = image[:, :, :3]
+        else:
+            image_rgb = image
 
-        return image
+        return image_rgb
 
-    def process(self, image_path, output_path, mask_path):
-        image_rgb = self.open(image_path)
-        self.mask(image_rgb, mask_path)
-        image_filled = self.fill(image_rgb)
-        # image_blured = self.blur(image_filled)
-        image_inpainted = self.inpaint(image_filled)
-        image_processed = self.save(image_inpainted, output_path)
+    def process(self, img_bytes, extension):
+        # Ensure the bytes are in a buffer format that OpenCV can understand
+        buffer = np.frombuffer(img_bytes, dtype=np.uint8)
+        img = cv2.imdecode(buffer, cv2.IMREAD_UNCHANGED)
 
-        return image_processed
+        image_rgb = self.remove_alpha(img)
+        image_masked = self.mask(image_rgb)  # masking
+        image_inpainted = self.inpaint(image_masked)  # inpainting
+        image_processed = self.combine_alpha(image_inpainted)
 
-    def processWithCheck(self, image_path, output_path, mask_path):
-        """
-        Execution time: 1.48 seconds
-        CPU Usage After: 19.8%
-        Memory Usage: 2.0%
-        """
-        import time
-        import psutil
+        _, mask_bytes = cv2.imencode("."+extension, self.masks.astype(np.uint8))
+        _, result_bytes = cv2.imencode("."+extension, image_processed)
+        return io.BytesIO(mask_bytes), io.BytesIO(result_bytes)
 
-        cpu_usage_before = psutil.cpu_percent(interval=None)
-        memory_usage_before = psutil.virtual_memory().percent
-        start_time = time.time()
+    '''
+    def blur(self, image_rgb, kernel_size=(601, 601)):
+            mask_3d = self.masks[:, :, None] == 0
+            blurred_image = cv2.GaussianBlur(image_rgb, kernel_size, 0)
+            image_rgb = np.where(mask_3d, image_rgb, blurred_image)
 
-        image_processed = self.process(self, image_path, output_path, mask_path) # 실행
-
-        end_time = time.time()
-        cpu_usage_after = psutil.cpu_percent(interval=1)
-        memory_usage_after = psutil.virtual_memory().percent
-
-        print(f"Execution time: {end_time - start_time:.2f} seconds")
-        print(f"CPU Usage After: {cpu_usage_after - cpu_usage_before}%")
-        print(f"Memory Usage: {memory_usage_after - memory_usage_before}%")
-
-        return image_processed
+            return image_rgb
+    '''
--- app/main.py
+++ app/main.py
@@ -1,63 +1,86 @@
 from uuid import uuid4
 from fastapi import FastAPI, HTTPException, File, UploadFile, Depends
-from pathlib import Path
-from starlette.responses import FileResponse
-import shutil
+from starlette.responses import StreamingResponse
 from app.ColorRemover import ColorRemover
-from app.ImageFunctions import Images
+import app.ImageFunctions as ImageManager
+from boto3 import client
+
+app = FastAPI()
+paths = dict()
+
+'''
+보안 주의
+'''
+BUCKET_NAME = "your bucket name"
+s3_client = client(
+        "s3",
+        aws_access_key_id="access key id",
+        aws_secret_access_key="secret access key",
+        region_name="region",
+    )
 
 
 def get_file_path():
     return paths
 
 
-def create_file_path(file_name):
+def create_file_path(file_name, extension):
     global paths
-
-    file_id = uuid4() # 각 클라이언트마다 고유한 파일 경로 생성
-    extension=file_name.split('.')[-1]
+    file_id = uuid4()  # 각 클라이언트마다 고유한 파일 ID 생성
+    extension = file_name.split('.')[-1]
     paths = {"input_path": f"./app/imgs/{file_id}.input.{extension}",
              "output_path": f"./app/imgs/{file_id}.output.{extension}",
-             "mask_path": f"./app/imgs/{file_id}_mask.{extension}",}
-
+             "mask_path": f"./app/imgs/{file_id}.mask.{extension}",
+             "extension": extension}
     return paths
 
 
-app = FastAPI()
-paths = dict()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 @app.post("/upload")
-async def upload(img: UploadFile = File(...)):
-    if img is None:
-        raise HTTPException(status_code=400, detail="No input file")
+async def upload(upload_file: UploadFile = File(...)):
+    try:
+        if upload_file is None:
+            raise HTTPException(status_code=400, detail="No input file")
+        upload_file, extension = await ImageManager.validate_type(upload_file)
+        upload_file = await ImageManager.validate_size(upload_file)
+        file_paths = create_file_path(upload_file.filename, extension)  # 경로 설정
+        s3_client.upload_fileobj(upload_file.file, BUCKET_NAME, file_paths["input_path"])
+    except Exception:
+        raise HTTPException(status_code=500, detail="File upload failed")
+    finally:
+        upload_file.file.close()
 
-    validated_file = await Images.validate_type(img)
-    validated_file = await Images.validate_size(img)
-    validated_file.file.seek(0)
-
-    file_paths = create_file_path(img.filename)
-    input_img_path = file_paths['input_path']
-
-    with open(input_img_path, "wb") as buffer:
-        shutil.copyfileobj(validated_file.file, buffer)
-
-    return {"message": "File uploaded successfully", "path": input_img_path}
-
+    return {"message": f"File {upload_file.filename} uploaded successfully",
+            "path": file_paths["input_path"]}
 
 @app.get("/process-color")
 def process_color(file_paths: dict = Depends(get_file_path)):
-    input_img_path = file_paths['input_path']
-    output_img_path = file_paths['output_path']
-    mask_path = file_paths['mask_path']
-
-    if not Path(input_img_path).exists():
-        raise HTTPException(status_code=404, detail=f"Input image not found")
+    """
+        color-based handwriting detection & Telea Algorithm-based inpainting
+    """
+    img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=file_paths['input_path'])  # download from S3
+    img_bytes = img_obj['Body'].read()
 
     target_rgb = (34, 30, 235)  # 221EEB in RGB
     color_remover = ColorRemover(target_rgb, tolerance=20)
-    color_remover.process(input_img_path, output_img_path, mask_path)
+    img_mask_bytes, img_output_bytes = color_remover.process(img_bytes, file_paths['extension'])
 
-    return FileResponse(output_img_path, media_type="image/" + output_img_path.split('.')[-1])
+    s3_client.upload_fileobj(img_mask_bytes, BUCKET_NAME, file_paths["mask_path"])
+    s3_client.upload_fileobj(img_output_bytes, BUCKET_NAME, file_paths["output_path"])
+
+    return {"message": "File processed successfully", "output": file_paths['output_path']}
+
+@app.get("/show/{file_type}")
+def show(file_type: str, file_paths: dict = Depends(get_file_path)):
+    """
+       Show images from S3 based on the file type: input, output, or mask.
+    """
+    if file_type not in ['input', 'output', 'mask']:
+        raise HTTPException(status_code=400, detail="Invalid file type specified.")
+    key_path = file_paths.get(f"{file_type}_path")
+
+    try:
+        img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=key_path)
+    except Exception:
+        raise HTTPException(status_code=500, detail=f"File {file_type} does not exist in here: {key_path}")
+
+    return StreamingResponse(content=img_obj['Body'], media_type="image/"+file_paths['extension'])
