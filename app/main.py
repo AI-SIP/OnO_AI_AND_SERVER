2fbaf40171409bd4c1f4c7ae46fece1215a4b223
semnisem <doldori102493@gmail.com>
2024-07-12 15:41:46 +0900

    s3 url에 의한 처리

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+import io
 from uuid import uuid4
+from urllib.parse import urlparse
+
+from botocore.exceptions import ClientError
 from fastapi import FastAPI, HTTPException, File, UploadFile, Depends
-from starlette.responses import StreamingResponse
+from starlette.responses import StreamingResponse, JSONResponse
 from app.ColorRemover import ColorRemover
 import app.ImageFunctions as ImageManager
 from boto3 import client
@@ -11,76 +15,114 @@ paths = dict()
 '''
 보안 주의
 '''
-BUCKET_NAME = "your bucket name"
+BUCKET_NAME = "myawsbucket-mvp"
 s3_client = client(
         "s3",
-        aws_access_key_id="access key id",
-        aws_secret_access_key="secret access key",
-        region_name="region",
-    )
+        aws_access_key_id="",
+        aws_secret_access_key="",
+        region_name="ap-northeast-2",
+)
+
+
+def create_file_path(obj_path, extension):
+    global paths
+    file_id = uuid4()  # 각 클라이언트마다 고유한 파일 ID 생성
+    dir_path = obj_path.rsplit('/', 1)[0]
+    paths = {"input_path": f"{dir_path}/{file_id}.input.{extension}",
+             "output_path": f"{dir_path}/{file_id}.output.{extension}",
+             "mask_path": f"{dir_path}/{file_id}.mask.{extension}",
+             "extension": extension}
+    print(paths)
 
 
 def get_file_path():
     return paths
 
 
-def create_file_path(file_name, extension):
-    global paths
-    file_id = uuid4()  # 각 클라이언트마다 고유한 파일 ID 생성
-    extension = file_name.split('.')[-1]
-    paths = {"input_path": f"./app/imgs/{file_id}.input.{extension}",
-             "output_path": f"./app/imgs/{file_id}.output.{extension}",
-             "mask_path": f"./app/imgs/{file_id}.mask.{extension}",
-             "extension": extension}
-    return paths
+def parse_s3_url(full_url: str):
+    """ URL에서 S3 키를 추출 """
+    parsed_url = urlparse(full_url)
+    return parsed_url.path.lstrip('/')
 
 
-@app.post("/upload")
-async def upload(upload_file: UploadFile = File(...)):
+def download_image_from_s3(s3_key: str):
+    """ S3에서 이미지를 동기적으로 다운로드 """
+    try:
+        img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=s3_key)
+        return img_obj['Body'].read()
+    except s3_client.exceptions.NoSuchKey:
+        raise HTTPException(status_code=404, detail=f"Image not found in S3 : {s3_key}.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+def upload_image_to_s3(file_bytes, file_path):
+    """ 이미지를 S3에 동기적으로 업로드 """
+    s3_client.upload_fileobj(file_bytes, BUCKET_NAME, file_path)
+
+
+@app.get("/process-color/{full_url:path}")
+def process_color(full_url: str):
+    """ color-based handwriting detection & Telea Algorithm-based inpainting """
+    print(full_url)
+    s3_key = parse_s3_url(full_url)
+    create_file_path(s3_key, s3_key.split(".")[-1])
+    img_bytes = download_image_from_s3(s3_key)  # download from S3
+
+    target_rgb = (34, 30, 235)  # 221EEB in RGB
+    color_remover = ColorRemover(target_rgb, tolerance=20)
+    img_mask_bytes, img_output_bytes = color_remover.process(img_bytes, paths['extension'])
+
+    upload_image_to_s3(io.BytesIO(img_bytes), paths["input_path"])
+    upload_image_to_s3(img_mask_bytes, paths["mask_path"])
+    upload_image_to_s3(img_output_bytes, paths["output_path"])
+
+    return JSONResponse(content={"message": "File processed successfully", "output": paths['output_path']})
+
+
+@app.get("/show-output")
+def showByOutput():
+    try:
+        img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=paths["output_path"])
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == 'NoSuchKey':
+            raise HTTPException(status_code=404, detail="File not found")
+        else:
+            raise HTTPException(status_code=500, detail=e)
+
+    return StreamingResponse(content=img_obj['Body'], media_type="image/" + paths["extension"])
+
+
+@app.get("/show-url")
+def showByUrl(full_url: str):
+    s3_key = parse_s3_url(full_url)
+    try:
+        img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=s3_key)
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == 'NoSuchKey':
+            raise HTTPException(status_code=404, detail="File not found")
+        else:
+            raise HTTPException(status_code=500, detail=f"500 error")
+
+    return StreamingResponse(content=img_obj['Body'], media_type="image/" + s3_key.split('.')[-1])
+
+
+@app.post("/direct/upload")
+async def upload_directly(upload_file: UploadFile = File(...)):
     try:
         if upload_file is None:
             raise HTTPException(status_code=400, detail="No input file")
         upload_file, extension = await ImageManager.validate_type(upload_file)
         upload_file = await ImageManager.validate_size(upload_file)
-        file_paths = create_file_path(upload_file.filename, extension)  # 경로 설정
-        s3_client.upload_fileobj(upload_file.file, BUCKET_NAME, file_paths["input_path"])
+        create_file_path(upload_file.filename, extension)  # 경로 설정
+        s3_client.upload_fileobj(upload_file.file, BUCKET_NAME, paths["input_path"])
     except Exception:
         raise HTTPException(status_code=500, detail="File upload failed")
     finally:
         upload_file.file.close()
 
     return {"message": f"File {upload_file.filename} uploaded successfully",
-            "path": file_paths["input_path"]}
+            "path": paths["input_path"]}
 
-@app.get("/process-color")
-def process_color(file_paths: dict = Depends(get_file_path)):
-    """
-        color-based handwriting detection & Telea Algorithm-based inpainting
-    """
-    img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=file_paths['input_path'])  # download from S3
-    img_bytes = img_obj['Body'].read()
-
-    target_rgb = (34, 30, 235)  # 221EEB in RGB
-    color_remover = ColorRemover(target_rgb, tolerance=20)
-    img_mask_bytes, img_output_bytes = color_remover.process(img_bytes, file_paths['extension'])
-
-    s3_client.upload_fileobj(img_mask_bytes, BUCKET_NAME, file_paths["mask_path"])
-    s3_client.upload_fileobj(img_output_bytes, BUCKET_NAME, file_paths["output_path"])
-
-    return {"message": "File processed successfully", "output": file_paths['output_path']}
-
-@app.get("/show/{file_type}")
-def show(file_type: str, file_paths: dict = Depends(get_file_path)):
-    """
-       Show images from S3 based on the file type: input, output, or mask.
-    """
-    if file_type not in ['input', 'output', 'mask']:
-        raise HTTPException(status_code=400, detail="Invalid file type specified.")
-    key_path = file_paths.get(f"{file_type}_path")
-
-    try:
-        img_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=key_path)
-    except Exception:
-        raise HTTPException(status_code=500, detail=f"File {file_type} does not exist in here: {key_path}")
-
-    return StreamingResponse(content=img_obj['Body'], media_type="image/"+file_paths['extension'])
