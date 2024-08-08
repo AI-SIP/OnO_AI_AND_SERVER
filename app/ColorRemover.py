46c6576adc6d6373cd11a8eea532c56e8b5d160d
semnisem <doldori102493@gmail.com>
2024-08-09 00:22:58 +0900

    feat: 원근 투영 변환 (scaling)
    컨투어 중 사각형 탐지하여 이미지의 50% 이상 크기일 경우 원근 투영 변환을 수행

--- app/ColorRemover.py
+++ app/ColorRemover.py
@@ -81,7 +81,9 @@ class ColorRemover:
         # Ensure the bytes are in a buffer format that OpenCV can understand
         buffer = np.frombuffer(img_bytes, dtype=np.uint8)
         img = cv2.imdecode(buffer, cv2.IMREAD_UNCHANGED)
+
         image_rgb = self.remove_alpha(img)
+        image_rgb = self.scaling(image_rgb)  # perspective transforming
 
         self.masking(image_rgb)  # masking
         image_inpainted = self.inpainting(image_rgb)  # inpainting
@@ -97,6 +99,111 @@ class ColorRemover:
         _, result_bytes = cv2.imencode("." + extension, image_output)
 
         return io.BytesIO(input_bytes), io.BytesIO(mask_bytes), io.BytesIO(result_bytes)
+
+
+    def scaling(self, img):
+        # buffer = np.frombuffer(img_bytes, dtype=np.uint8)
+        # img = cv2.imdecode(buffer, cv2.IMREAD_UNCHANGED)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # gray scale로 변환
+        gray_image = cv2.equalizeHist(gray_image)
+        gray_image = cv2.GaussianBlur(gray_image, (15, 15), 0)
+        gray_image = cv2.bilateralFilter(gray_image, 9, 75, 75)  # 엣지 보존 필터 적용
+        gray_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                           cv2.THRESH_BINARY, 115, 4)  # 적응형 임계값 적용
+        gray_image = cv2.medianBlur(gray_image, 11)  # 중앙값 필터 적용
+        gray_image = cv2.copyMakeBorder(gray_image, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=[0, 0, 0])  # 이미지 테두리 추가
+
+        edges = cv2.Canny(gray_image, 100, 200)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        # edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+        contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+        # logging.info(f"contours found: {contours}")
+        contour_image = img.copy()
+        # cv2.drawContours(contour_image, contours, -1, (0, 255, 0), 5)  # 상위 5개 컨투어를 녹색으로 그림
+
+        screenCnt = None
+        for c in contours:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            if len(approx) == 4:
+                screenCnt = approx
+                break
+
+        if screenCnt is None:
+            logging.warning("No contour found")
+            newImage = img  # 변환에 실패한 경우 원본 이미지 반환
+        else:
+            cv2.drawContours(contour_image, [screenCnt], -1, (0, 255, 0), 5)  # 사각 컨투어를 녹색으로 그림
+            logging.info(f"Rectangle Contour Found")
+            newImage = self.four_point_transform(img, screenCnt.reshape(4, 2))
+
+        # _, edges_bytes = cv2.imencode("." + extension, edges)
+        # _, contour_bytes = cv2.imencode("." + extension, cv2.cvtColor(contour_image, cv2.COLOR_RGB2BGR))
+        # _, result_bytes = cv2.imencode("." + extension, cv2.cvtColor(newImage, cv2.COLOR_BGR2RGB))
+
+        # return io.BytesIO(edges_bytes), io.BytesIO(contour_bytes), io.BytesIO(result_bytes)
+        return newImage
+
+    def four_point_transform(self, image, pts):
+        def order_points(pts):
+            r = np.zeros((4, 2), dtype="float32")
+            s = pts.sum(axis=1)
+            diff = np.diff(pts, axis=1)
+            r[0] = pts[np.argmin(s)]
+            r[2] = pts[np.argmax(s)]
+            r[1] = pts[np.argmin(diff)]
+            r[3] = pts[np.argmax(diff)]
+            return r
+
+        rect = order_points(pts)
+        (tl, tr, br, bl) = rect
+        widthA = np.linalg.norm(br - bl)
+        widthB = np.linalg.norm(tr - tl)
+        maxWidth = max(int(widthA), int(widthB))
+        heightA = np.linalg.norm(tr - br)
+        heightB = np.linalg.norm(tl - bl)
+        maxHeight = max(int(heightA), int(heightB))
+
+        docArea = maxWidth * maxHeight
+        orgArea = image.shape[0] * image.shape[1]
+        logging.info(f"\ndoc area: ({maxWidth},{maxHeight})={docArea},\norg area: {image.shape}={orgArea}")
+        if docArea > orgArea*0.5:
+            logging.info("transformed")
+            dst = np.array([
+                [0, 0],
+                [maxWidth, 0],
+                [maxWidth, maxHeight],
+                [0, maxHeight]
+            ], dtype="float32")
+
+            M = cv2.getPerspectiveTransform(rect, dst)
+            warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        else:
+            logging.info("originals")
+            warped = image
+
+        return warped
+
+    def background_filtering(self, img_rgb, extension):
+        image_filtered = img_rgb.copy()
+        hsv = cv2.cvtColor(image_filtered, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+
+        hist_s, _ = np.histogram(s, bins=256, range=(0, 256))
+        dominant_s = np.argmax(hist_s)
+        hist_v, _ = np.histogram(v, bins=256, range=(0, 256))
+        dominant_v = np.argmax(hist_v)
+
+        gray_mask = (s < dominant_s + 15) & (v > dominant_v - 60)
+        v[gray_mask] = 255
+        s[gray_mask] = 0
+        image_hsv = cv2.merge([h, s, v])
+        image_filtered = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+        return image_filtered
 
 
 
